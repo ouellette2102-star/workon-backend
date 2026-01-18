aef1d1a775e86e4bfb1b206e10d7592cc6e2e6d2
Mathieu Ouellette <mathieu@example.com>
2026-01-18 00:18:59 -0500

    fix(reviews): import AuthModule for JwtAuthGuard dependency

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReviewsController } from './reviews.controller';
 import { ReviewsService } from './reviews.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [ReviewsController],
   providers: [ReviewsService],
   exports: [ReviewsService],
